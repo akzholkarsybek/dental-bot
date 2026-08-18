520d408d969b7c47b74d919db5e9da35823865a3
Akzhol Karsybek <aka@MacBook-Air-Akzhol.local>
2026-08-18 17:03:12 +0500

    feat: containerize dental bot

---
+++ seed.sql
@@ -0,0 +1,21 @@
+INSERT INTO services
+    (name, description, price, duration_minutes)
+VALUES
+    (
+        'Teeth Cleaning',
+        'Professional dental cleaning',
+        25000,
+        60
+    ),
+    (
+        'Dental Consultation',
+        'General dental consultation',
+        15000,
+        30
+    ),
+    (
+        'Dental Filling',
+        'Treatment of a cavity',
+        30000,
+        60
+    );
